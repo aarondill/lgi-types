da3546be3c1ff0d2c9ecf3732ad275eb70880b6a
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-15 12:49:12 -0500

    fix: type GVarient

--- lgi/GLib/GVariant.lua
+++ lgi/GLib/GVariant.lua
@@ -1,3 +1,6 @@
 ---@class GVariantStatic TODO: missing constructors
+---@field new fun(type:string, value:unknown[]):GVariant
 
 ---@class GVariant TODO: missing fields
+---@field type string
+---@field value unknown
