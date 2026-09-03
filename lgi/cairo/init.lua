2d1011b4078ffa385576c702911792b1fdefb84f
Aaron Dill <aaronsacks2006@gmail.com>
2026-09-02 23:29:25 -0500

    feat: add cairo.pattern (no fields)

--- lgi/cairo/init.lua
+++ lgi/cairo/init.lua
@@ -1,5 +1,6 @@
 ---@class cairo
 ---@field Surface CairoSurfaceStatic
+---@field Pattern CairoPatternStatic
 ---@field version integer
 ---@field version_encode fun(major: integer, minor: integer, micro: integer): integer
 ---@field version_string string
@@ -7,3 +8,8 @@
 ---@class CairoSurfaceStatic :LgiClassStatic
 ---@class CairoSurface :LgiClass
 ---@field finish fun(self: CairoSurface)
+
+---@class CairoPatternStatic :LgiClassStatic
+---@class CairoPattern :LgiClass
+
+return require("lgi").cairo
